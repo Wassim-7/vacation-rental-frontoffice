2f8bf3664b9fc427cda777409e23405d8cabbc9a
Wassim Ben Taleb <wassimwbt@gmail.com>
2021-07-01 12:11:00 +0100

    fix margin

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -43,7 +43,7 @@ function Home() {
         return (
           <Card
             hoverable
-            style={{ width: 300, margin: 8 }}
+            style={{ width: 300, margin: 7 }}
             cover={<img alt="example" style={{
               maxHeight: '300px',
               maxWidth: '300px',
